9a8f6942d16507d6a2c64279f3b7b0e70cf861ea
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-03-20 08:43:14 +0600

    recapped useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
@@ -12,16 +13,24 @@ function App() {
 }
 
 const style = {
-  backgroundColor: 'yellow',
+  backgroundColor: 'bisque',
   borderRadius: '20px',
-  margin: '20px'
+  margin: '20px',
+  padding: '20px'
 };
 
 function Person(props) {
+  const [age, handleAge] = useState(0);
+  const increaseAge = () => {
+    const newAge = age + 1;
+    handleAge(newAge); /* Sending the new age to hook */
+  }
   return (
     <div style={style}>
       <h1>Name: {props.name}</h1>
       <p>Job: {props.job}</p>
+      <p>Age: {age}</p>
+      <button onClick={increaseAge/* Not calling function here as it will call the function even if button is not clicked */}>Increase Age</button>
     </div>
   )
 }
